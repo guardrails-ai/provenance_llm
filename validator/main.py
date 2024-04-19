8ad11221f46e0804bde213da255cab5528309c22
Wyatt Lansford <22553069+wylansford@users.noreply.github.com>
2024-04-18 18:33:30 -0700

    switch to openai embeddings

--- validator/main.py
+++ validator/main.py
@@ -8,6 +8,7 @@ from warnings import warn
 
 import nltk
 import numpy as np
+import openai
 from guardrails.stores.context import get_call_kwarg
 from guardrails.utils.docs_utils import get_chunks_from_text
 from guardrails.utils.validator_utils import PROVENANCE_V1_PROMPT
@@ -67,6 +68,11 @@ class ProvenanceLLM(Validator):
             top_k=top_k,
             **kwargs,
         )
+        from openai import OpenAI
+
+        openai.api_key = os.environ.get("OPENAI_API_KEY")
+
+        self.embedding_client = OpenAI()
 
         if validation_method not in ["sentence", "full"]:
             raise ValueError("validation_method must be 'sentence' or 'full'.")
@@ -262,51 +268,64 @@ class ProvenanceLLM(Validator):
         If `query_function` is provided, it will be used. Otherwise, `sources` and
         `embed_function` will be used to create a default query function.
         """
-        query_fn = metadata.get("query_function", None)
+        # query_fn = metadata.get("query_function", None)
         sources = metadata.get("sources", None)
 
-        # Check that query_fn or sources are provided
-        if query_fn is not None:
-            if sources is not None:
-                warnings.warn(
-                    "Both `query_function` and `sources` are provided in metadata. "
-                    "`query_function` will be used."
+        # # Check that query_fn or sources are provided
+        # if query_fn is not None:
+        #     if sources is not None:
+        #         warnings.warn(
+        #             "Both `query_function` and `sources` are provided in metadata. "
+        #             "`query_function` will be used."
+        #         )
+        #     return query_fn
+
+        # if sources is None:
+        #     raise ValueError(
+        #         "You must provide either `query_function` or `sources` in metadata."
+        #     )
+
+        # # Check chunking strategy, size and overlap
+        # chunk_strategy = metadata.get("chunk_strategy", "sentence")
+        # if chunk_strategy not in ["sentence", "word", "char", "token"]:
+        #     raise ValueError(
+        #         "`chunk_strategy` must be one of 'sentence', 'word', "
+        #         "'char', or 'token'."
+        #     )
+        # chunk_size = metadata.get("chunk_size", 5)
+        # chunk_overlap = metadata.get("chunk_overlap", 2)
+
+        # # Check embed model
+        # embed_function = metadata.get("embed_function", None)
+        # if embed_function is None:
+        #     # Load model for embedding function
+        #     MODEL = SentenceTransformer("paraphrase-MiniLM-L6-v2")
+
+        #     # Create embed function
+        #     def st_embed_function(sources: list[str]):
+        #         return MODEL.encode(sources)
+
+        #     embed_function = st_embed_function
+
+        def embed_function(text, model="text-embedding-3-small"):
+            text = text[-1]
+            text = text.replace("\n", " ")
+            result = np.array(
+                (
+                    self.embedding_client.embeddings.create(input=[text], model=model)
+                    .data[0]
+                    .embedding
                 )
-            return query_fn
-
-        if sources is None:
-            raise ValueError(
-                "You must provide either `query_function` or `sources` in metadata."
             )
+            return result
 
-        # Check chunking strategy, size and overlap
-        chunk_strategy = metadata.get("chunk_strategy", "sentence")
-        if chunk_strategy not in ["sentence", "word", "char", "token"]:
-            raise ValueError(
-                "`chunk_strategy` must be one of 'sentence', 'word', "
-                "'char', or 'token'."
-            )
-        chunk_size = metadata.get("chunk_size", 5)
-        chunk_overlap = metadata.get("chunk_overlap", 2)
-
-        # Check embed model
-        embed_function = metadata.get("embed_function", None)
-        if embed_function is None:
-            # Load model for embedding function
-            MODEL = SentenceTransformer("paraphrase-MiniLM-L6-v2")
-
-            # Create embed function
-            def st_embed_function(sources: list[str]):
-                return MODEL.encode(sources)
-
-            embed_function = st_embed_function
         return partial(
             self.query_vector_collection,
             sources=metadata["sources"],
             embed_function=embed_function,
-            chunk_strategy=chunk_strategy,
-            chunk_size=chunk_size,
-            chunk_overlap=chunk_overlap,
+            chunk_strategy="sentence",
+            chunk_size=5,
+            chunk_overlap=2,
         )
 
     @staticmethod
